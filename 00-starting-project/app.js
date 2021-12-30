e561b0f20356a3d4154a2c60332babd02602ee5d
Plamen Petrov <biznetmen@gmail.com>
2021-12-30 17:19:40 +0200

    more app.js refactoring

--- 00-starting-project/app.js
+++ 00-starting-project/app.js
@@ -35,7 +35,10 @@ app.post("/recommend", function (req, res) {
 });
 
 app.get("/restaurants", function (req, res) {
-  res.render('restaurants');
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  res.render('restaurants', {numberOfRestaurants: storedRestaurants.length});
 });
 
 app.listen(3000);
