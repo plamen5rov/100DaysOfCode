dcad7010734f0b3fc0f3d03bab585e0a2083ed05
Plamen Petrov <biznetmen@gmail.com>
2021-12-30 12:38:21 +0200

    minor editing

--- 00-starting-project/app.js
+++ 00-starting-project/app.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", function (req, res) {
+app.get('/', function(req, res) {
   res.send(<h1>Server running!</h1>);
 });
 
