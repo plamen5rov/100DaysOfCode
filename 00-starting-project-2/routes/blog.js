56d69d600739bbc53c2a62ca5e7985a4e25fd127
Plamen Petrov <biznetmen@gmail.com>
2022-01-10 16:08:03 +0200

    blog routes added

---
+++ 00-starting-project-2/routes/blog.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.redirect('/posts');
+});
+
+router.get('/posts', function(req, res) {
+    res.render('posts-list');
+});
+
+module.exports = router;
