73aaa2ecbc78f25020f2bbb4f10f9bb191ef22de
Plamen Petrov <biznetmen@gmail.com>
2022-01-11 12:09:00 +0200

    router post implemented

--- 00-starting-project-2/routes/blog.js
+++ 00-starting-project-2/routes/blog.js
@@ -3,17 +3,29 @@ const db = require('../data/database');
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
+router.get('/posts', function (req, res) {
     res.render('posts-list');
 });
 
-router.get('/new-post', async function(req, res) {
+router.get('/new-post', async function (req, res) {
     const [authors] = await db.query('SELECT * FROM blog.authors');
-    res.render('create-post', {authors: authors});
+    res.render('create-post', { authors: authors });
+});
+
+router.post('/posts', async function (req, res) {
+    const data = [
+        req.body.title,
+        req.body.summary,
+        req.body.content,
+        req.body.author
+    ];
+    await db.query('INSERT INTO blog.posts (title, summary, body, author_id) VALUES (?)', [data]);
+
+    res.redirect('/posts');
 });
 
 module.exports = router;
