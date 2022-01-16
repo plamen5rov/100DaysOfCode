4a8faddb676d10243b33482b092999ed696583e7
Plamen Petrov <biznetmen@gmail.com>
2022-01-16 11:32:38 +0200

    post deleting implemented

--- 00-starting-project-2/routes/blog.js
+++ 00-starting-project-2/routes/blog.js
@@ -99,6 +99,13 @@ router.post('/posts/:id/edit', async function (req, res) {
 
 });
 
+router.post('/posts/:id/delete', async function (req, res) {
+    const query = `DELETE FROM blog.posts WHERE id = ?`;
 
+    await db.query(query, [req.params.id]);
+
+    res.redirect('/posts');
+
+});
 
 module.exports = router;
