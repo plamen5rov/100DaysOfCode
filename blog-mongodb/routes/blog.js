feff8fa476c01c25b33c0d4f5955bcf64312bef5
Plamen Petrov <biznetmen@gmail.com>
2022-02-01 15:32:15 +0200

    Adding New Post to DB implemented

--- blog-mongodb/routes/blog.js
+++ blog-mongodb/routes/blog.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
+const mongodb = require('mongodb');
 const db = require('../data/database');
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -16,6 +17,28 @@ router.get('/new-post', async function (req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();
 
   res.render('create-post', { authors: authors });
+});
+
+router.post('/posts', async function(req, res) {
+
+  const authorID = new ObjectId(req.body.author);
+  const author = await db.getDb().collection('authors').findOne({ _id: authorID });
+  
+  const newPost = {
+    title: req.body.title,
+    summary: req.body,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorID,
+      name: author.name,
+      email: author.email
+    }
+  };
+  const result = await db.getDb().collection('posts').insertOne(newPost);
+  
+  res.redirect('/posts');
+
 });
 
 module.exports = router;
