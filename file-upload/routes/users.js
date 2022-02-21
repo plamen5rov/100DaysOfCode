3617d93865ba795a39b8aaab946ff36e522b8ba6
Plamen Petrov <biznetmen@gmail.com>
2022-02-21 12:32:19 +0200

    storing files on the backednd with mutler implemented

--- file-upload/routes/users.js
+++ file-upload/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 
-const upload = multer({});
+const upload = multer({ dest: 'images'});
 const router = express.Router();
 
 
@@ -13,9 +13,14 @@ router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
 
-router.post('/profiles', upload.single('image'),function(req, res){
+router.post('/profiles', upload.single('image'), function(req, res){
   const uploadedImageFile = req.file;
   const userData = req.body;
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+
+  res.redirect('/');
   
 });
 module.exports = router;
