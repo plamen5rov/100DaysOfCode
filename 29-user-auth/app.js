135f888ab54615b65ed0e819375a866518e35125
Plamen Petrov <biznetmen@gmail.com>
2022-04-27 10:58:01 +0300

    connect-mongodb-session npm package installed

--- 29-user-auth/app.js
+++ 29-user-auth/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const session = require('express-session');
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
@@ -12,6 +13,13 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: 'super-secret',
+  resave: false,
+  saveUninitialized: false,
+  store: 
+}));
 
 app.use(demoRoutes);
 
