9b679829bb7f35bd77110474479cb603bded3641
Plamen Petrov <biznetmen@gmail.com>
2022-05-02 13:16:47 +0300

    Session Input Data Verification [498]

--- 29-user-auth/routes/demo.js
+++ 29-user-auth/routes/demo.js
@@ -10,7 +10,20 @@ router.get('/', function (req, res) {
 });
 
 router.get('/signup', function (req, res) {
-  res.render('signup');
+
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: '',
+      confirmEmail: '',
+      password: ''
+    };
+  }
+  req.session.inputData = null;
+  
+  res.render('signup', { inputData: sessionInputData });
 });
 
 router.get('/login', function (req, res) {
@@ -31,7 +44,18 @@ router.post('/signup', async function (req, res) {
     !enteredEmail.includes('@')
   ) {
     console.log('Incorrect data entered!');
-    return res.redirect('/signup');
+
+    req.session.inputData = {
+      hasError: true,
+      message: 'Invalid input - please, check your data!',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    };
+    req.session.save(function () {
+      res.redirect('/signup');
+    });
+    return;
   }
 
   const existingUser = await db
@@ -103,6 +127,6 @@ router.post('/logout', function (req, res) {
   req.session.user = null;
   req.session.isAuthenticated = false;
   res.redirect('/');
- });
+});
 
 module.exports = router;
